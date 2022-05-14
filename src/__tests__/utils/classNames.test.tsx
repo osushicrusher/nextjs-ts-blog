bf5ce77bea54423b67975c96136445471717c880
h.yoshino <osushicrusher@gmail.com>
2022-05-14 18:01:29 +0900

    chore: changed test

--- src/__tests__/utils/classNames.test.tsx
+++ src/__tests__/utils/classNames.test.tsx
@@ -2,7 +2,7 @@ import classNames from '@/utils/classNames';
 
 describe('Check if classes can be expanded', () => {
   test('Check if classes can be expanded when using ternary operators', () => {
-    const tabIndex = 1;
-    expect(classNames(tabIndex === 1 ? 'aaa' : 'bbb ccc')).toBe('aaa');
+    const tabIndex = 2;
+    expect(classNames(tabIndex === 2 ? 'aaa' : 'bbb ccc')).toBe('aaa');
   });
 });
